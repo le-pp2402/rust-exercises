6a3823f43dbbc1640efbfaec3be4e65fd94d420e
root <root@PC202507BR665.BRYCENVN.LOCAL>
2025-12-22 15:44:31 +0700

    39/94

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -6,7 +6,12 @@ struct Point {
 
 #[derive(Debug)]
 enum Message {
-    // TODO: Define the different variants used below.
+    // TODO: Define a few types of messages as used below.
+    Resize { width: u32, height: u32 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
